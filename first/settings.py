7634ba139754276823bb49eab914fa860d2ca9ac
Your Name <2585803+tt5@users.noreply.github.com>
2021-10-09 20:16:57 +0200

    adde hello to installed apps£

--- first/settings.py
+++ first/settings.py
@@ -24,13 +24,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'hello.apps.HelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello',
 ]
 
 MIDDLEWARE = [

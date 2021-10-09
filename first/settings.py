11d2bbbeeefbd39c423277d5ab150e3a8a61eca2
Your Name <2585803+tt5@users.noreply.github.com>
2021-10-09 21:21:07 +0200

    postgres

--- first/settings.py
+++ first/settings.py
@@ -119,3 +119,6 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

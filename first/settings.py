23d88e898fd96d9451b0afb46e44475fb1df7efa
Your Name <2585803+tt5@users.noreply.github.com>
2021-10-09 19:11:22 +0200

    settings

--- first/settings.py
+++ first/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
